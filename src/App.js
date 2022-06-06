5f93e138dd569ca5691bba1295dfa78e4f25408d
xorne <xorne@protonmail.ch>
2022-06-06 20:02:08 +0200

    contract fix

--- src/App.js
+++ src/App.js
@@ -140,7 +140,7 @@ function App() {
     setFeedback(`Minting your ${CONFIG.NFT_NAME}...`);
     setClaimingNft(true);
     blockchain.smartContract.methods
-      .mint(mintAmount)
+      .Mint(mintAmount)
       .send({
         gasLimit: String(totalGasLimit),
         to: CONFIG.CONTRACT_ADDRESS,
